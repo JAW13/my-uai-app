174184c3219c26b9cdcee33eb399735375ab0520
JAW13 <joaquin.arancibia.walker@gmail.com>
2023-10-03 23:18:12 -0300

    Descripción de los cambios realizados

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer(){
     return(
       <footer class="w-full bg-gray-300 p-8">
     <div class="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-gray-300 text-center md:justify-between">
-    <a href="" className="flex items-center gap-1">
+    <a href="https://www.google.com/" className="flex items-center gap-1">
              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 -rotate-90">
              <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
              </svg>
